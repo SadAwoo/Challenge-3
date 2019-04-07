a89cd29b9fa4096fba3b18b92a8af7ae2c50fd5a
SadAwoo <46636256+SadAwoo@users.noreply.github.com>
2019-04-07 15:48:42 -0400

    added code
    added code

---
+++ GameController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text ScoreText;
+    public Text restartText;
+    public Text gameOverText;
+    public Text winText;
+
+    private int score;
+    private bool gameOver;
+    private bool restart;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        winText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+    if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.T))
+            {
+                SceneManager.LoadScene("Space Shooter"); // or whatever the name of your scene is
+            }
+        }
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range (0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'T' Try Again";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            winText.text = "You win!";
+            gameOverText.text = "GAME CREATED BY STEPHEN BECK";
+            gameOver = true;
+            restart = true;
+        }
+    }
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
